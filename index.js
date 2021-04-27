a667078cdb291defb258c4ca75f730f65f06cd2e
tati89 <malena.one@gmail.com>
2021-04-27 14:37:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Description
+// This time we're going to build a train fare calculator.
+// Simple enough, right? We're not looking for something too fancy,
+//  but you should be able to input your age and the distance you're travelling
+//  and get your ticket price.
+
+// Instructions
+// - Ask the user for their age and how far they are travelling (in km)
+// - The price per travelled km will be £0.21
+// - Junior passengers (under 18) get a 20% discount
+// - Senior passengers (over 65) get a 40% discount
+
+// Challenge
+// We're going to rewrite our program so that the user chooses a starting point
+// and a destination from a list of stations, besides inputting their age and then
+//  get the ticket price.
+
+// You can come up with the stations' names and distances. Try also to code it so
+// that you only need to change the price per km in one place, and all the stations
+//  should have the updated price
